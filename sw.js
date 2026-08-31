9db6e4b8acbc3a0923a73d76853f57067cf1f8e8
amarparamanik777-pixel <amarparamanik777@gmail.com>
2026-08-31 16:45:19 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'study-log-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((names) => Promise.all(
+      names.filter((n) => n !== CACHE).map((n) => caches.delete(n))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
